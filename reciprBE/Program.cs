e81186310f1ec9361972b0e527a479f46e93b543
PiqqiDesigns <eric.otuvaka@gmail.com>
2023-03-21 14:28:49 -0700

    fix: CORS error bypass

--- reciprBE/Program.cs
+++ reciprBE/Program.cs
@@ -4,11 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IMealService, MealService>();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(builder => {
+    builder.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
  
 app.UseExceptionHandler("/error");
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); 
 app.MapControllers();
 app.Run();
 
